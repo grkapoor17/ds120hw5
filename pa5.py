21dbaecb629a345fe82ae23e4df78a57640ccb42
grkapoor17 <grkapoor@uchicago.edu>
2024-05-07 17:22:17 -0500

    Improved style

--- pa5.py
+++ pa5.py
@@ -1,21 +1,23 @@
-import math
 
-def gcd(a,b):
-    if b == 0:
-        return a
-    else:
-        bigger = max(a,b)
-        smaller = min(a,b)
-        return gcd(smaller, bigger % smaller)
+
+def gcd(val1, val2):
+    if val2 == 0:
+        return val1
+
+    bigger = max(val1, val2)
+    smaller = min(val1, val2)
+    return gcd(smaller, bigger % smaller)
+
 
 def remove_pairs(path_str):
     if len(path_str) <= 1:
         return path_str
-    
+
     if is_opposite(path_str[0], path_str[1]):
         return remove_pairs(path_str[2:])
-    else:
-        return path_str[0] + remove_pairs(path_str[1:])
+
+    return path_str[0] + remove_pairs(path_str[1:])
+
 
 def is_opposite(dir1, dir2):
     if dir1 == "E" and dir2 == "W":
@@ -24,10 +26,12 @@ def is_opposite(dir1, dir2):
         return True
     if dir1 == "W" and dir2 == "E":
         return True
-    if dir1 == "S" and dir2 == "N":
-        return True
-    else:
-        return False
+    # if dir1 == "S" and dir2 == "N":
+    #     return True
+    # else:
+    #     return False
+    return (dir1 == "S" and dir2 == "N")
+
 
 def bisection_root(func, guess1, guess2):
     if abs(func(guess1)) <= 0.001:
@@ -38,13 +42,16 @@ def bisection_root(func, guess1, guess2):
         raise ValueError("Both y-values are the same sign")
     if func(guess1) < 0 and func(guess2) < 0:
         raise ValueError("Both y-values are the same sign")
-    
+
     new_guess = (guess1 + guess2) / 2
     new_y = func(new_guess)
 
-    if (new_y >= 0 and func(guess1) < 0) or (new_y < 0 and func(guess1) >= 0):
+    if new_y * func(guess1) < 0:
         return bisection_root(func, guess1, new_guess)
-    elif (new_y >= 0 and func(guess2) < 0) or (new_y < 0 and func(guess2) >= 0):
-        return bisection_root(func, new_guess, guess2)
-    else:
-        raise ValueError("Both y-values are the same sign")
+
+    return bisection_root(func, new_guess, guess2)
+    # else:
+    #     raise ValueError("Both y-values are the same sign")
+
+# print(bisection_root(lambda x: 1.33 -x , 1, 3))
+# print(bisection_root(lambda x: x*x - 3, 1, 4))
